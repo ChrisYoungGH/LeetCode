c7523b594edc51b4e7300f3e00f3fa5d19dad3f7
ChrisYoungGH <chrisyoung_yrk@163.com>
2017-08-18 19:41:50 +0800

    version 1

---
+++ 463.IslandPerimeter/perimeter.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int perimeter = 0;
+        for (int i = 0; i < m; i++) {
+             for (int j = 0; j < n; j++) {
+                 if (grid[i][j] == 1) {
+                     perimeter += 4;
+                     if (i > 0 && grid[i-1][j] == 1) perimeter--;
+                     if (i < m-1 && grid[i+1][j] == 1) perimeter--;
+                     if (j > 0 && grid[i][j-1] == 1) perimeter--;
+                     if (j < n-1 && grid[i][j+1] == 1) perimeter--;
+                 }
+             }
+        }
+
+        return perimeter;
+    }
+};
