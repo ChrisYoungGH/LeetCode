be22aa3ab1b7e8a5ebc320e86b753534bf13ef6f
ChrisYoungGH <chrisyoung_yrk@163.com>
2017-08-21 01:38:08 +0800

    version 1

---
+++ 507.PerfectNumber/perfect.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum = 1;
+        int fac = 2;
+        while (fac <= num / fac) {
+            if (num % fac == 0) {
+                sum += fac + num / fac;
+            }
+            fac++;
+        }
+        if (num > 1 && sum == num) {
+            return true;
+        }
+        return false;
+    }
+};
